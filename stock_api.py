05c6df7c0066c9622b1885bc25985f2823a16b75
Juro_lin <119565933+FoolApe@users.noreply.github.com>
2024-01-05 16:13:15 +0800

    Update stock_api.py

--- stock_api.py
+++ stock_api.py
@@ -1,19 +1,29 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 import twstock
 
 app = Flask(__name__)
 
 def get_stock_info(stock_code):
-    stock = twstock.realtime.get(stock_code)
-    chk = stock['success']
-
-    if chk:
-        openPrice = round(float(stock['realtime']['open']), 1)
-        highPrice = round(float(stock['realtime']['high']), 1)
-        lowPrice = round(float(stock['realtime']['low']), 1)
-        return openPrice, highPrice, lowPrice
-    else:
-        return None
+    if ',' in stock_code:
+        stock_codes = stock_code.split(',')
+        stock_data = {}
+        for code in stock_codes:
+            stock = twstock.realtime.get(code.strip())
+            success = stock.get('success', False)
+            if success:
+                open_price = round(float(stock['realtime']['open']), 2)
+                high_price = round(float(stock['realtime']['high']), 2)
+                low_price = round(float(stock['realtime']['low']), 2)
+                stock_name = stock['info']['name']
+                stock_data[code] = {
+                    'stockName': stock_name,
+                    'openPrice': str(open_price),
+                    'highPrice': str(high_price),
+                    'lowPrice': str(low_price)
+                }
+            else:
+                stock_data[code] = None
+        return stock_data
 
 @app.route('/get_stock_info', methods=['GET'])
 def stock_info():
@@ -22,11 +32,11 @@ def stock_info():
         return jsonify({'error': 'Stock code is required'})
 
     result = get_stock_info(stock_code)
+
     if result:
-        openPrice, highPrice, lowPrice = result
-        return jsonify({'openPrice': openPrice, 'highPrice': highPrice, 'lowPrice': lowPrice})
+        return jsonify(result)
     else:
         return jsonify({'error': 'Unable to retrieve stock information'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=80)
